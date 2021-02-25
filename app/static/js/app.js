f9b9f31d25b0691a5a284b9d3990f1585a8bf5a4
Matthieu O <mat.ortlieb@gmail.com>
2021-02-25 14:50:15 +0100

    first tests with Map API and flask routes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,20 +9,49 @@ $(function () {
                 this.contentString = "Vous: " + contentString;
             }
             else {
+                parsing(content);
                 this.contentString = "GrandPyBot: " + contentString;
             }
-            // return contentString;
         }
 
     }
 
     let button = $('#button');
 
-    button.on('click', function () {
+    button.on('click', function (e) {
+        e.preventDefault();
         let userInput = $('#userQuestion').val();
-        alert(userInput);
         let messages = new message('user', userInput);
         let userMessage = ("<div class='userMessage'>" + messages.contentString + "</div>");
+        answer_zone.css('display', 'block')
         answer_zone.append(userMessage);
-    })
-})
+    });
+
+    let map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        });
+    }
+
+
+    // function parsing(textToParse) {
+    //     $.ajax({
+    //         url: 'http://127.0.0.1:5000/parser/',
+    //         data: textToParse,
+    //         type: 'POST',
+    //         success: function (response) {
+    //             console.log("worked");
+    //         },
+    //         error: function (error) {
+    //             console.log(error);
+    //         }
+    //     });
+    // }
+
+    // let a = parsing("Test text to be parsed");
+    // let apiResponse = ("<div class='apiResponse'>" + a + "</div>");
+    // answer_zone.append(apiResponse);
+});
--- app/views.py
+++ app/views.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello world 235!"
+    return render_template('index.html')
 
 
 @app.route('/test32')
 def test32():
     return "Test32563"
+
+
+@app.route('/parser', methods=['GET', 'POST'])
+def parser():
+    userInput = str(request.form.get('question'))
+    userMessDisplay = "Vous: " + str(request.form.get('question'))
+    apiAnswer = "GrandPyBot: " + userInput
+    return render_template('index.html', parsedTextDisplayed=apiAnswer, userQuestion=userMessDisplay)
 
 
 if __name__ == "__main__":
