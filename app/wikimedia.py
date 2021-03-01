c534039619fff07a48484a756c036f65655f951b
Matthieu O <mat.ortlieb@gmail.com>
2021-03-01 17:12:26 +0100

    page title from wikimedia retrieved

--- app/wikimedia.py
+++ app/wikimedia.py
@@ -8,24 +8,40 @@ import json
 # https://fr.wikipedia.org/api/rest_v1/page/summary/Tour%20Eiffel
 
 
-def get_wikimedia_page_title(location):
-    # for letter in location:
-    #     if letter == " ":
-    #         letter = "%20"
-    #     else:
-    #         pass
-    # print(location)
-    # # location_no_space
-    title_api_url = (
-        """https://fr.wikipedia.org/w/api.php?
-        action=query&prop=revisions&rvslots=*&rvprop=content
-        &formatversion=2&format=json&titles="""
-        + location)
+def eliminate_space_char(location):
+    """
+    Replace the space character with %20
+    Mandatory to insert location in   the api request
+    """
+    char_pos = 0
 
-    print(title_api_url)
-    exact_page_title = requests.get(title_api_url)
-    title_json = json.loads(exact_page_title.content.decode('utf-8'))
-    print(title_json)
+    for letter in location:
+        if letter == " ":
+            newLocation = location[:char_pos] + "%20"+location[char_pos+1:]
+            char_pos += 1
+            print("The location has changed from {} to {}".format(
+                location, newLocation))
+        else:
+            char_pos += 1
+    return newLocation
+
+
+def get_wikimedia_page_title(location):
+    """
+    Retrieve the exact name of the page to access its content
+    """
+    title_api_url = (
+        "https://fr.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&format=json&srwhat=nearmatch&srsearch="
+        + location)
+    # print(title_api_url)
+    exact_page_infos = requests.get(title_api_url)
+    # print(exact_page_infos.text)
+    infos_json = json.loads(exact_page_infos.content.decode('utf-8'))
+    # print(infos_json)
+    exact_page_title = infos_json["query"]["search"][0]["title"]
+    print("\none matching page found: {}".format(exact_page_title))
+
+
 # self.product_name = p_json["products"][prod]["product_name_fr"]
 # if "nutriscore_grade" in p_json["products"][prod]:
 #     self.nutriscore = p_json["products"][prod]["nutriscore_grade"]
@@ -37,4 +53,5 @@ def get_wikimedia_page_title(location):
 
 
 if (__name__ == "__main__"):
-    get_wikimedia_page_title("tour_eiffel")
+    correct_input = eliminate_space_char("tour eiffel")
+    get_wikimedia_page_title(correct_input)
