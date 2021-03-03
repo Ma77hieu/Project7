3d849c9b0e762dafe54e8edb8c124b6282784f90
Matthieu O <mat.ortlieb@gmail.com>
2021-03-03 11:06:17 +0100

    added loader

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,25 +1,9 @@
 
-class message {
-    constructor(author, content) {
-        let contentString = (content);
-        if (author == 'user') {
-            this.contentString = "Vous: " + contentString;
-        }
-        else {
-            parsing(content);
-            this.contentString = "GrandPyBot: " + contentString;
-        }
-    }
-
-}
-
-
-
 $(document).ready(function () {
     let map;
     let marker
     const answer_zone = $('#answer');
-
+    const loader = $('#loader');
 
     function initMap(myPos) {
         map = new google.maps.Map(document.getElementById("map"), {
@@ -34,31 +18,32 @@ $(document).ready(function () {
 
     $('form').on('submit', function (e) {
         e.preventDefault();
+        loader.show();
         $.ajax({
             url: 'parser',
             type: "POST",
             success: function (responseJSON) {
-                console.log(responseJSON)
+                // console.log(responseJSON)
                 let itemLat = responseJSON.latitude
-                console.log(itemLat)
+                // console.log(itemLat)
                 let itemLon = responseJSON.longitude
-                console.log(itemLon)
+                // console.log(itemLon)
                 let userQuestion = responseJSON.userMessage
                 let parsedTextDisplayed = responseJSON.apiAnswer
-                console.log(userQuestion)
-                console.log(parsedTextDisplayed)
+                // console.log(userQuestion)
+                // console.log(parsedTextDisplayed)
                 let userMessage = ("<div class='userMessage'>" + userQuestion + "</div>");
                 let apiResponse = ("<div class='apiResponse'>" + parsedTextDisplayed + "</div>");
                 answer_zone.css('display', 'block')
                 answer_zone.append(userMessage);
                 answer_zone.append(apiResponse);
-                console.log('ca marche');
+                // console.log('ca marche');
                 let myPos = { lat: itemLat, lng: itemLon };
                 initMap(myPos);
             },
             error: function () {
-                console.log('oops');
+                console.log('oops, something went wrong');
             }
-        })
+        }).done(function () { loader.hide(); })
     })
 })
--- app/views.py
+++ app/views.py
@@ -16,29 +16,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/test32')
-def test32():
-    return "Test32563"
-
-
 @app.route('/parser', methods=['POST'])
 def parser():
-    # userInput = textToParse
-    # userInput = request.form["question"]
-    # print(request.form["question"])
+    userInput = request.form.get("question")
+    print(userInput)
     userInput = "Bonsoir Grandpy, j'espère que tu as passé une belle semaine. Est-ce que tu pourrais m'indiquer l'adresse de la tour eiffel? Merci d'avance et salutations à Mamie."
-    # print(request.form['question'])
     print(userInput)
     userQuestion = "<strong>Vous:</strong> " + userInput
     parsed_user_input = parse(userInput)
     print("PARSED:{}".format(parsed_user_input))
     print("TRIMMED: {}".format(trim(parsed_user_input)))
-
     correct_input = trimSpace(trim(parsed_user_input))
     summary = getSummary(correct_input)
+    parsedTextDisplayed = "<strong>GrandPyBot:</strong> " + summary
     lat = getCoordinates(correct_input)[0]
     lon = getCoordinates(correct_input)[1]
-    parsedTextDisplayed = "<strong>GrandPyBot:</strong> " + summary
     return jsonify(userMessage=userQuestion,
                    apiAnswer=parsedTextDisplayed,
                    latitude=lat,
